93b52398a76c803de22f348f0d9cc0ece9e2a867
jxp <x>
2025-04-24 18:13:36 +0100

    Added a search icon

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,6 +8,10 @@ export default function Home() {
 
   async function handleSubmit() {
     console.log(inputValue);
+    if (inputValue.length === 0) {
+      console.log('Input is empty');
+      return;
+    }
     try {
       const url = new URL(`http://localhost:8000/search`);
       url.searchParams.append('query', inputValue);
@@ -17,6 +21,7 @@ export default function Home() {
           'Content-Type': 'application/json',
         },
       });
+
       if (data.ok) {
         let json = await data.json();
         console.log(json);
@@ -30,8 +35,9 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <form onSubmit={(e) => { e.preventDefault(); e.stopPropagation(); handleSubmit() }}>
+    <div className="flex flex-row items-center justify-center h-screen">
+      <form className='flex flex-row items-center justify-center'
+        onSubmit={(e) => { e.preventDefault(); e.stopPropagation(); handleSubmit() }}>
         <input
           className="border rounded px-4 py-2 w-100"
           type="text"
@@ -40,6 +46,11 @@ export default function Home() {
           onChange={(e) => { e.preventDefault(); e.stopPropagation(); setInputValue(e.target.value); }}
           placeholder="Ask a question" />
       </form>
+      <button onClick={(e) => { e.preventDefault(); e.stopPropagation(); handleSubmit() }} className="mx-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+      </button>
     </div>
   );
 }
