4ec2f052282518b95a27f5cafccb7ea0bb0d7cbf
jxp <x>
2025-04-25 16:56:09 +0100

    Added navigation bar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,6 +9,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <div className="h-16 flex space-x-4 items-center justify-center text-2xl font-bold text-black/40 p-10">
+          <Link className="hover:text-black/80" href="/">Explore</Link>
+          <span className="text-6xl" >&#xB7;</span>
+          <Link className="hover:text-black/80" href="/trails">Trails</Link>
+        </div>
         {children}
       </body>
     </html>
