003e8eedabe600544ef38506ed48ad088cd505de
jxp <x>
2025-04-28 11:53:35 +0100

    Reformat trails pages

--- frontend/src/app/trails/[id]/page.tsx
+++ frontend/src/app/trails/[id]/page.tsx
@@ -25,26 +25,30 @@ export default async function TrailItemPage({ params }: {
 
     return (
         <div className="mx-auto w-screen h-screen px-1 md:px-2 flex flex-col items-center">
-            <div className="flex flex-row max-w-5xl w-full items-top p-4 gap-10">
-                <div className="flex flex-col w-4/6 space-y-2 m-2">
+            <div className="max-w-5xl">
+                <div className="flex flex-col justify-left w-full px-4 my-4">
                     <h1 className="font-semibold text-2xl text-black">{trail.narrative?.title || trail.name || "Untitled"}</h1>
                     <p>{formatLocalDate(trail.created_at)}</p>
-                    <Image src="/narrative_image.png" alt="narrative image" width={600} height={200} className="rounded-lg shadow-md" />
-                    <Markdown>{trail.narrative?.content}</Markdown>
-                    {/* <p className="flex-grow">{trail.summary || "A meandering and lovely trail through your notes"}</p> */}
                 </div>
-                <div className="flex flex-col mx-auto p-1 md:px-2 w-2/6 m-2">
-                    <ul className="grid gap-y-3">
-                        {trail.nodes.map((d, i) => (
-                            <li className="flex-1" key={i}>
-                                <div
-                                    className="space-y-5 hover:text-white hover:cursor-pointer flex flex-col bg-sulphur-yellow/50 hover:bg-carmine-red/80 transition-colors duration-200 shadow-md rounded-lg p-4">
-                                    <h2 className="text-lg font-bold line-clamp-2 mb-2">{d.title || "Untitled"}</h2>
-                                    <p>{d.content}</p>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                <div className="flex flex-row w-full items-top px-4 gap-10">
+                    <div className="flex flex-col w-4/6 space-y-2">
+                        <Image src="/narrative_image.png" alt="narrative image" width={600} height={200} className="rounded-lg shadow-md" />
+                        <Markdown>{trail.narrative?.content}</Markdown>
+                        {/* <p className="flex-grow">{trail.summary || "A meandering and lovely trail through your notes"}</p> */}
+                    </div>
+                    <div className="flex flex-col mx-auto p-1 md:px-2 w-2/6 m-2">
+                        <ul className="grid gap-y-3">
+                            {trail.nodes.map((d, i) => (
+                                <li className="flex-1" key={i}>
+                                    <div
+                                        className="space-y-5 hover:text-white hover:cursor-pointer flex flex-col bg-sulphur-yellow/50 hover:bg-carmine-red/80 transition-colors duration-200 shadow-md rounded-lg p-4">
+                                        <h2 className="text-lg font-bold line-clamp-2 mb-2">{d.title || "Untitled"}</h2>
+                                        <p>{d.content}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
