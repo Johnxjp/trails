eb6dc211d96d02453755cf5698523b610d4da37a
jxp <x>
2025-04-28 11:06:43 +0100

    Small size change to cdot

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <body className={`${satoshi_sans.variable}`}>
         <div className="h-16 flex space-x-4 items-center justify-center text-2xl font-bold text-black/40 p-10">
           <Link className="hover:text-black/80" href="/">Explore</Link>
-          <span className="text-6xl" >&#xB7;</span>
+          <span className="text-2xl" >&#xB7;</span>
           <Link className="hover:text-black/80" href="/trails">Trails</Link>
         </div>
         {children}
